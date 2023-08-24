90300fe9303fa920bd21a6054a3c1a78d764d64d
JJvr2023 <129042030+JJvr2023@users.noreply.github.com>
2023-08-24 09:17:11 +0200

    Update push.c

--- push.c
+++ push.c
@@ -1,42 +1,6 @@
 #include "monty.h"
-
-/**
- * f_push - add node to the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-
-void f_push(stack_t **head, unsigned int counter)
-{
-	int n, j = 0, flag = 0;
-
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
-=======
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Stack {
   int data[STACK_SIZE];
@@ -58,7 +22,7 @@ void f_push(Stack **head, unsigned int counter) {
     (*head)->top = 0;
   }
 
-  (*head)->data[(*head)->top++] = value;
+  (*head)->data[++(*head)->top] = value;
 }
 
 int main(void) {
@@ -76,4 +40,3 @@ int main(void) {
 
   return 0;
 }
-
